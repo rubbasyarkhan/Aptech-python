d671609ddfd3dfe1f9484603330549cca262e214
Rubbas yar khan <rubbasyarkhan007@gmail.com>
2025-12-29 10:09:24 +0500

    hello world array

--- index.py
+++ index.py
@@ -1,14 +1,8 @@
 # print("hello world")
 
-arr=["grape","apple","banana","orange"]
-print(arr)
+# arr=["grape","apple","banana","orange"]
+# print(arr)
 
 # arr[0]="kiwi"
 
-# print(arr)
 
-arr.reverse()
-print(arr)
-
-arr.remove("apple")
-print(arr)
