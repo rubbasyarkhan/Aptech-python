acc4669454680b303369241620bfbc9a99546af1
Rubbas yar khan <rubbasyarkhan007@gmail.com>
2025-12-29 10:10:17 +0500

    dictionary , tuple ,

--- index.py
+++ index.py
@@ -6,3 +6,40 @@
 # arr[0]="kiwi"
 
 
+# print(arr)
+
+# arr.reverse()
+# print(arr)
+
+# arr.remove("apple")
+# print(arr)
+
+# numberarray=[1,5,3,5,8,5,5,4,7,2,6]
+# print(numberarray)
+# numberarray.sort()
+# print(numberarray)
+# print(numberarray.count(5))
+
+# tuple can not be changed but list can be changed
+# tuple=(1,2,3,4,5)
+# tuple=tuple+(6,)
+# print(tuple)    
+
+
+# in python dictionary is a key value pair and it is mutable .
+dictionary={"name":"John","age":36,"city":"New York","country":"USA","gender":"male","married":False}
+# print(dictionary)
+dictionary["name"]="jane"
+# print(dictionary)
+# dictionary.pop("age")
+# print(dictionary)
+dictionary.clear()
+print(dictionary)
+
+# for key,value in dictionary.items():
+#     print(key,value)
+
+
+
+
+
